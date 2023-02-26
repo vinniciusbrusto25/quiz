4c2e484512088bdbe734f2f923c50d0b56a76fd7
Vinicius Brustolin <vinnicius_brusto@hotmail.com>
2023-02-26 18:58:50 -0300

    Aplicando estilo a Questao
    - Usando os mesmos conceitos do CSS como os atributos da font, text-align, margin, entre outros;
    - Usando um conteiner p/ envolver o componente Questao.

--- lib/questao.dart
+++ lib/questao.dart
@@ -8,6 +8,21 @@ class Questao extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(this.texto);
+    return Container(
+      //valor máximo do double. É uma das estratégias p/ deixar no máximo
+      //Dá pra pegar a largura da tela, mas nesse caso então pegando o máximo
+      //do Double
+      width: double.infinity,
+      //fromLTRB representa left top right bottom, ou simplesmente usar
+      //all que aplica a todos da mesma forma como no CSS
+      margin: EdgeInsets.all(8),
+      child: Text(
+        this.texto,
+        style: TextStyle(
+          fontSize: 22
+        ),
+        textAlign: TextAlign.center
+      ),
+    );
   }
 }
